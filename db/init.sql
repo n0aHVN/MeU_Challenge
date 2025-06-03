5d76b132486d14807359d605a20c09dad4847931
n0aHVN <superherodung123@gmail.com>
2025-06-03 13:59:54 +0700

    Finish challenge 3

---
+++ db/init.sql
@@ -0,0 +1,50 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE racket (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    brand VARCHAR(100) NOT NULL,
+    description VARCHAR(5000) NOT NULL,
+    racket_name VARCHAR(100) NOT NULL,
+    slug VARCHAR(100) NOT NULL,
+    weight INTEGER,
+    speed_rating FLOAT CHECK (speed_rating >= 0),
+    vibration_rating FLOAT CHECK (vibration_rating >= 0),
+    composition VARCHAR(255),
+    racket_size VARCHAR(20),
+    thickness VARCHAR(20),
+    price INT CHECK (price >= 0) NOT NULL,
+    quantity INT CHECK (price >= 0) NOT NULL,
+    status VARCHAR(50) CHECK (status IN ('disable', 'enable')) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE OR REPLACE FUNCTION set_update_at_column()
+RETURNS TRIGGER AS 
+$$
+BEGIN
+	NEW.updated_at = CURRENT_TIMESTAMP;
+	RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER set_update_trigger 
+BEFORE UPDATE ON racket
+FOR EACH ROW
+EXECUTE FUNCTION set_update_at_column();
+
+INSERT INTO racket (brand, racket_name, slug, description, speed_rating, vibration_rating, weight, composition, racket_size, thickness, price, quantity,status)
+VALUES
+('Butterfly', 'Zhang Jike ALC', 'zhang-jike-alc', 'This is the description', 11.8, 10.3, 82, '5 Wood Layers + 2 Arylate Carbon Layers', '157x150mm', '5.8mm', 100, 8000000, 'enable'),
+('Butterfly', 'Viscaria ALC', 'viscaria-alc', 'This is the description', 11.8, 10.3, 82, '5 Wood Layers + 2 Arylate Carbon Layers', '157x150mm', '5.8mm', 100, 3200000, 'enable'),
+('Butterfly', 'Amultart ZLC', 'amultart-zlc', 'This is the description', 12.2, 13.7, 76, '3 Wood Layers + 2 Arylate Carbon Layers', '157x150mm', '7.1mm', 100, 3800000, 'enable'),
+('Butterfly', 'Fan Zhendong ALC', 'fan-zhendong-alc', 'This is the description', 11.8, 10.3, NULL, '5 Wood Layers + 2 Arylate Carbon Layers', '157x150mm', '5.8mm', 100, 3000000, 'enable'),
+('Butterfly', 'Zhang Jike ALC NDN', 'zhang-jike-alc-ndn', 'This is the description', 11.8, 10.3, 82, '5 Wood Layers + 2 Arylate Carbon Layers', '157x150mm', '5.8mm', 100, 9000000, 'enable'),
+('Butterfly', 'Timo Boll ALC', 'timo-boll-alc', 'This is the description', 11.8, 10.3, 85, '5 Wood Layers + 2 Carbon Layers', '157x150mm', '5.8mm', 100, 3000000, 'disable');
+
+CREATE USER tt_admin WITH PASSWORD '123';
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ttadmin;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO ttadmin;
