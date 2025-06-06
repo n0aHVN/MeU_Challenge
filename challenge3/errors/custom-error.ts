efd37857b91abde9c09af4fefa2a8dce05dd7d3c
n0aHVN <superherodung123@gmail.com>
2025-06-06 10:43:46 +0700

    Delete Challenge 3

--- challenge3/errors/custom-error.ts
+++
@@ -1,8 +0,0 @@
-export abstract class CustomError extends Error {
-    constructor(msg: string) {
-        super(msg);
-        Object.setPrototypeOf(this, CustomError.prototype);
-    }
-    abstract statusCode: number;
-    abstract serializeErrors(): { message: string; field?: string }[];
-}
